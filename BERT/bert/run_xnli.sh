1e123b3bef1e07877788da13d998dbed65a63fb4
Stanwang1210 <ch995308@gmail.com>
2020-09-28 15:58:14 +0800

    'upload'

--- BERT/bert/run_xnli.sh
+++ BERT/bert/run_xnli.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python run_xnli.py --data_dir /work/b07u1234/b07701209/HW4_1_BERT/HW4_1_1/BERT/dataset \
+CUDA_VISIBLE_DEVICES=0 python run_xnli.py --data_dir /work/b07u1234/data_public/HW4_1_Data/HW4_1_1/dataset \
     --model_type bert \
     --model_name_or_path bert-base-chinese \
     --output_dir ../output/try \
